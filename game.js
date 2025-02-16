e70c1a64457810de65404da45ea21a500c1fb3a3
manar mustafa <abuhaitmanar@gmail.com>
2025-02-16 21:59:48 +0300

    new

--- game.js
+++ game.js
@@ -1,3 +1,40 @@
+var buttonColours = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+var userClickedPattern = []
+
+
 function nextSequence(){
     var randomNumber = Math.floor(Math.random()* 4);
+    var randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+
+       playSound(randomChosenColour);
 }
+
+$(document).keypress(function(){
+    nextSequence();
+});
+
+$(".btn").click(function(){
+    var userChosenColour = $(this).attr("id");
+    userClickedPattern.push(userChosenColour);
+    console.log(userClickedPattern);
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
+});
+
+function playSound(name){
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+}
+
+
+function animatePress(currentColour){
+    $("#" + currentColour).addClass("pressed");
+    setTimeout(() => {
+        $("#" + currentColour).removeClass("pressed");
+    }, 100);
+    }
+
